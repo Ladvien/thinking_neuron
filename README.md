90499f250429e4fdf5200887ad433af7af32ee9d
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2025-02-20 07:14:36 -0600

    Working on RPi

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # neuron_template
 A template for the Neuron projects
+
+
+## Setup Ollama on Ubuntu
+1. Install `ollama`
+```sh
+curl -fsSL https://ollama.com/install.sh | sh
+```
+2. 
--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import requests
 
-HOST = "http://0.0.0.0:8000"
+HOST = "http://192.168.1.140:8000"
 url = f"{HOST}/think"
 
 response = requests.post(url, json={"text": "What's up dude?"}, stream=True)
